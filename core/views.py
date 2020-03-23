394bc5236f6111356e33282f58b0f615d12f396f
Some Name <ewigeis@gmail.com>
2020-03-23 16:04:55 +0300

    add Item, Order, OrderItem models

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Item
+
 
 def home(request):
-	return render(request, 'core/home.html')
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'core/home.html', context)
 
 # Create your views here.
